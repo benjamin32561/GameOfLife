b920821bc12f215f673385416aabe8e1ae6d1cf9
benjamin32561 <ben32561@gmail.com>
2025-10-20 11:48:25 +0300

    continued, changed some logic, broke to cleaner funcs

--- entities/base_object_class.py
+++ entities/base_object_class.py
@@ -17,22 +17,6 @@ def validate_coordinates(x, y, grid):
         return False
     return True
 
-def get_all_possible_steps(grid, x, y):
-    """
-    Get all possible steps (step size is 1) for a given position.
-
-    args:
-        grid: the grid object of the simulation
-        x: the x position of the entity
-        y: the y position of the entity
-    returns:
-        list of all possible steps
-    """
-    x_range = range(max(0, x - 1), min(len(grid), x + 2))
-    y_range = range(max(0, y - 1), min(len(grid[0]), y + 2))
-    all_possible_steps = [(x, y) for x, y in product(x_range, y_range) if validate_coordinates(x, y, grid)]
-    return all_possible_steps
-
 class GOLGrid:
     def __init__(self, init_file_path=None):
         """
@@ -125,11 +109,47 @@ class GOLGrid:
             predator = self.create_predator(x, y)
             self.grid[x][y].append(predator)
     
+    def get_all_possible_steps(self, x, y):
+        """
+        Get all possible steps (step size is 1) for a given position.
+
+        args:
+            x: the x position of the entity
+            y: the y position of the entity
+        returns:
+            list of all possible steps
+        """
+        x_range = range(max(0, x - 1), min(self.width, x + 2))
+        y_range = range(max(0, y - 1), min(self.height, y + 2))
+        all_possible_steps = [(x, y) for x, y in product(x_range, y_range) if validate_coordinates(x, y, grid)]
+        return all_possible_steps
+
+    def get_all_cells_with_type(self, object_type):
+        """
+        Get all cell coordinates with an object of a given type.
+
+        args:
+            object_type: the type of object to find
+        returns:
+            list of cell coordinates
+        """
+        to_ret = []
+        for x, y in product(range(self.width), range(self.height)):
+            if len(self.grid[x][y]) > 0 and any(isinstance(entity, object_type) for entity in self.grid[x][y])
+                to_ret.append((x,y))
+        return to_ret
+
     def get_all_empty_cells(self):
         """
         Get all empty cells in the grid.
         """
         return [(x, y) for x, y in product(range(self.width), range(self.height)) if len(self.grid[x][y]) == 0]
+
+    def is_object_type_in_cell(self, x, y, object_type):
+        """
+        Check if an object of a given type is in a given cell.
+        """
+        return len(self.grid[x][y]) > 0 and any(isinstance(entity, object_type) for entity in self.grid[x][y])
 
     def randomly_add_object(self, object_to_add):
         """
@@ -181,8 +201,9 @@ class GOLGrid:
         """
         updated_grid = [[[] for _ in range(self.width)] for _ in range(self.height)]
         for x, y in product(range(self.width), range(self.height)):
-            for entity in self.grid[x][y]:
-                to_keep, new_x, new_y = entity.update(self.grid)
+            while self.grid[x][y]:
+                entity = self.grid[x][y].pop() # remove current entity so it will ignore it's own existence when updating
+                to_keep, new_x, new_y = entity.update(self)
                 if to_keep:
                     updated_grid[new_x][new_y].append(entity)
         self.grid = updated_grid
@@ -219,7 +240,7 @@ class Entity:
         """
         self.ttl = self.base_ttl
 
-    def get_next_position(self, grid):
+    def get_next_position(self, gol_grid):
         """
         Get the new position of the object.
 
@@ -230,7 +251,7 @@ class Entity:
         """
         pass
 
-    def update(self, grid):
+    def update(self, gol_grid):
         """
         Update the object.
 
@@ -256,20 +277,20 @@ class Plant(Entity):
         """
         super().__init__(x, y, base_ttl)
     
-    def get_next_position(self, grid):
+    def get_next_position(self, gol_grid):
         """
         Get the new position of the plant.
         Plants don't move, so the new position is the same as the current position.
         """
         return self.x, self.y
     
-    def update(self, grid):
+    def update(self, gol_grid):
         """
         Update the plant.
         Plants don't move, but age and can be eaten.
         
         args:
-            grid: The grid object of the simulation.
+            gol_grid: The grid object of the simulation.
         returns:
             keep: a boolean indicating if the object should be kept in the simulation.
         """
@@ -298,66 +319,69 @@ class MobileEntity(Entity):
         super().__init__(x, y, base_ttl)
         self.sight_radius = sight_radius
 
-    def get_random_step(self, grid):
+    def get_random_step(self, gol_grid):
         """
         Get a random step for a given position.
 
         args:
-            grid: the grid object of the simulation
+            gol_grid: the grid object of the simulation
         returns:
             the x and y coordinates of the random step
         """
-        all_possible_steps = get_all_possible_steps(grid, self.x, self.y)
+        all_possible_steps = gol_grid.get_all_possible_steps(self.x, self.y)
         return random.choice(all_possible_steps) 
 
-    def find_closest_object_coordinates(self, grid, object_type):
+    def get_closest_plant_coordinates(self, gol_grid, object_type):
         """
         find the closest object of a given type in a given radius
 
         args:
-            x: x position
-            y: y position
-            grid: the grid object of the simulation
+            gol_grid: the grid object of the simulation
             object_type: the type of object to find
 
         returns:
             the x and y position of the closest object
             -1, -1 if no object is found
         """
-        for i, j in product(range(-self.sight_radius, self.sight_radius), range(-self.sight_radius, self.sight_radius)):
-            if not validate_coordinates(self.x + i, self.y + j, grid):
-                continue
-            if grid[self.x + i][self.y + j] is not None and isinstance(grid[self.x + i][self.y + j], object_type):
-                return self.x + i, self.y + j
-        return -1, -1     
+        all_cells_with_plants = gol_grid.get_all_cells_with_type(Plant)
+        
+        # get closest one in a radius
+        closest_coords = (-1, -1)
+        closest_distance = float('inf')
+        for x, y in all_cells_with_plants:
+            distance = math.sqrt((self.x-x)**2+(self.y-y)**2)
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_coords = (x, y)
+        return closest_coords
 
-    def get_next_position_towards_object(self, grid, x, y):
+    def get_next_position_towards_object(self, gol_grid, x, y):
         """
         Get the next position moving to the nearest 
 
         args:
-            grid: the grid object of the simulation
+            gol_grid: the grid object of the simulation
             x: the x position of the desired position to move towards
             y: the y position of the desired position to move towards
         returns:
             the x and y coordinates of the next position
         """
-        all_possible_steps = get_all_possible_steps(grid, x, y)
+        all_possible_steps = gol_grid.get_all_possible_steps(grid, x, y)
         closest_step = None
         closest_distance = float('inf')
         for step in all_possible_steps:
-            distance = abs(step[0] - x) + abs(step[1] - y)
+            distance = math.sqrt((step[0]-x)**2+(step[1]-y)**2)
             if distance < closest_distance:
                 closest_distance = distance
                 closest_step = step
         return closest_step
 
-    def get_next_position(self, grid, object_type):
+    def get_next_position(self, gol_grid, object_type):
         """
         Find the closest object of a given type in a given radius.
 
         args:
-            grid: The grid object of the simulation.
+            gol_grid: The grid object of the simulation.
             object_type: The type of object to find.
         returns:
             The x and y coordinates of the closest object.
@@ -390,12 +414,12 @@ class Herbivore(MobileEntity):
         """
         self.T_cooldown = self.base_T_cooldown
     
-    def update(self, grid):
+    def update(self, gol_grid):
         """
         Update the herbivore.
 
         args:
-            grid: the grid object of the simulation
+            gol_grid: the grid object of the simulation
         returns:
             keep: a boolean indicating if the object should be kept in the simulation.
             new_x: the new x position of the object.
@@ -404,19 +428,79 @@ class Herbivore(MobileEntity):
         if self.should_be_removed():
             return False, -1, -1  # Remove from simulation
         
-        
+        # reduce ttl
+        self.ttl -= 1
+
         # move towards the closest plant
         new_x, new_y = self.get_next_position(grid, Plant)
         
-        #check if next position contains a predator, if so, return False, -1, -1
-        if len(grid[new_x][new_y]) > 0 and isinstance(grid[new_x][new_y][0], Predator):
+        # check if next position contains a predator, if so, die
+        if len(grid[new_x][new_y]) > 0 and gol_grid.is_object_type_in_cell(x, y, Predator):
             return False, -1, -1
-        
-        # check if next position contains a herbivore, if so, reproduce
-        if len(grid[new_x][new_y]) > 0 and isinstance(grid[new_x][new_y][0], Herbivore):
-            self.reproduce(grid)
-            return True, self.x, self.y
+                
+        # check if next position contains a herbivore, if so, reproduce, staying in the same space and spawning another herbivore in a random neighboring cell.
+        if len(grid[new_x][new_y]) > 0 and gol_grid.is_object_type_in_cell(x, y, Herbivore):
+            # check if any other herbivore is in the same cell, if so,
+            # check if they can both reproduce, reset their cooldown and create a new herbivore
+            other_herbivores = gol_grid.is_object_type_in_cell(new_x, new_y, Herbivore)
+            
+            if len(other_herbivores) >= 1:  # At least one other herbivore in the cell
+                # Check if both herbivores can reproduce (cooldown is 0)
+                can_reproduce = (self.T_cooldown <= 0 and 
+                                all(herb.T_cooldown <= 0 for herb in other_herbivores))
+                
+                if can_reproduce:
+                    # Reset cooldowns for all herbivores involved
+                    self.reset_mating_cooldown()
+                    for herb in other_herbivores:
+                        herb.reset_mating_cooldown()
+                    
+                    # Create new herbivore in a random neighboring cell
+                    success = grid.add_herbivore_to_random_place(new_x, new_y)
+                    
+            return True, new_x, new_y
 
+        # check if next position has a plant in it, if so 
+        if len(grid[new_x][new_y]) > 0 and isinstance(grid[new_x][new_y][0], Plant):
+            grid.grid[new_x][new_y] = [obj for obj in grid.grid[new_x][new_y] if not isinstance(obj, Plant)]
+
+        return True, new_x, new_y
+
+class Predator(MobileEntity):
+    def __init__(self, x, y, base_ttl=None, sight_radius=None):
+        """
+        Predator object class.
+        """
+        super().__init__(x, y, base_ttl, sight_radius)
+    
+    def update(self, gol_grid):
+        """
+        Update the predator.
+
+        args:
+            gol_grid: the grid object of the simulation
+        returns:
+            keep: a boolean indicating if the object should be kept in the simulation.
+            new_x: the new x position of the object.
+            new_y: the new y position of the object.
+        """
+        if self.should_be_removed():
+            return False, -1, -1  # Remove from simulation
+        
         # reduce ttl
         self.ttl -= 1
+        
+        # move towards the closest Herbivore
+        new_x, new_y = self.get_next_position(grid, Herbivore)
+        
+        # check if next position contains a herbivore, if so, remove it and reinit the ttl of the predator
+        if len(grid[new_x][new_y]) > 0 and isinstance(grid[new_x][new_y][0], Herbivore):
+            grid.grid[new_x][new_y] = [obj for obj in grid.grid[new_x][new_y] if not isinstance(obj, Herbivore)]
+            self.reset_ttl()
+            return True, new_x, new_y
+
+        # check if next position has a plant in it, if so, delete it
+        if len(grid[new_x][new_y]) > 0 and isinstance(grid[new_x][new_y][0], Plant):
+            grid.grid[new_x][new_y] = [obj for obj in grid.grid[new_x][new_y] if not isinstance(obj, Plant)]
+
         return True, new_x, new_y
